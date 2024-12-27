9ab4bbf7a05feb4aa4c46052dfce197520b1e994
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-12-27 19:25:22 +0100

    Update authentication mechanism to use session authorization based on access token

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,44 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    //Write the authenication mechanism here
+  if (req.session && req.session.accessToken) {
+    jwt.verify(
+      req.session.accessToken,
+      "fingerprint_customer",
+      (err, decoded) => {
+        if (err) {
+          return res.status(401).json({ message: "Unauthorized access" });
+        }
+        req.user = decoded;
+        next();
+      }
+    );
+  } else {
+    return res.status(401).json({ message: "Unauthorized access" });
+  }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
