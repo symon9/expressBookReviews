295737ca28fa3fc8a3dd1a1e0304c544659a14b6
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-12-27 21:28:47 +0100

    Task 4: Implement route to get book details based on title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -43,7 +43,14 @@ public_users.get('/author/:author', function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const booksByTitle = Object.values(books).filter(book => book.title === title );
+  
+  if(booksByTitle.length > 0){
+    return res.status(200).json(booksByTitle);
+  }else{
+    return res.status(404).json({message: "Books by this title not found"});
+  }
 });
 
 //  Get book review
