db4e130fb49537930a065f8d68c7a209c4d1e494
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-12-27 21:43:13 +0100

    Task 6: Complete user registration endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,7 +5,18 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  if (users[username]) {
+    return res.status(409).json({ message: "Username already exists" });
+  }
+
+  users[username] = { password };
+  return res.status(201).json({ message: "User registered successfully" });
 });
 
 // Get the book list available in the shop
