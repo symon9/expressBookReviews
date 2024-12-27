46825829e2573e3b1ef7ada7c3afeb7cdb1f747c
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-12-27 21:35:14 +0100

    Task 5: Implement route to get book reviews based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,7 +45,7 @@ public_users.get('/title/:title',function (req, res) {
   //Write your code here
   const title = req.params.title;
   const booksByTitle = Object.values(books).filter(book => book.title === title );
-  
+
   if(booksByTitle.length > 0){
     return res.status(200).json(booksByTitle);
   }else{
@@ -55,8 +55,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  
+  if(book && book.reviews){
+    return res.status(200).json(book.reviews);
+    }else{
+      return res.status(404).json({message: "Reviews not found for this book"});
+    }
 });
 
 module.exports.general = public_users;
