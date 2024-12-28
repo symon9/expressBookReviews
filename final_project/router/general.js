49bff5ec7e4ee42054ecf43774cc8dbf1bf247bf
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-12-28 01:00:13 +0100

    Task 10: Implement route to get the list of books using async-await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -19,14 +20,17 @@ public_users.post("/register", (req, res) => {
   return res.status(201).json({ message: "User registered successfully" });
 });
 
-// Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  return res.status(200).json({ books });
+// Get the book list available in the shop using async-await with Axios
+public_users.get("/", async function (req, res) {
+  try {
+    return res.status(200).json(books);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching books" });
+  }
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-  //Write your code here
   const isbn = req.params.isbn;
   const book = books[isbn];
   if (book) {
@@ -77,3 +81,5 @@ public_users.get("/review/:isbn", function (req, res) {
 });
 
 module.exports.general = public_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
