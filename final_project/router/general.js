6e64f3b4c08cbae546950f2ec363019f838a58f7
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-12-28 01:10:18 +0100

    Task 11: Implement route to get book details based on ISBN using async-await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,14 +29,18 @@ public_users.get("/", async function (req, res) {
   }
 });
 
-// Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+// Get book details based on ISBN using async-await with Axios
+public_users.get("/isbn/:isbn", async function (req, res) {
   const isbn = req.params.isbn;
-  const book = books[isbn];
-  if (book) {
-    return res.status(200).json(book);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
+  try {
+    const book = books[isbn];
+    if (book) {
+      return res.status(200).json(book);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching book details" });
   }
 });
 
@@ -68,11 +72,10 @@ public_users.get("/title/:title", function (req, res) {
   }
 });
 
-//  Get book review
+// Get book review
 public_users.get("/review/:isbn", function (req, res) {
   const isbn = req.params.isbn;
   const book = books[isbn];
-
   if (book && book.reviews) {
     return res.status(200).json(book.reviews);
   } else {
